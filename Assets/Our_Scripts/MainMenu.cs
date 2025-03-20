7ef26e24babaad2f662d745c773ec0f1dcaff643
TimCarrot <info@timleonardullrich.de>
2025-03-20 15:17:01 +0300

    rudimentary Main Menu finished

--- Assets/Our_Scripts/MainMenu.cs
+++ Assets/Our_Scripts/MainMenu.cs
@@ -6,5 +6,11 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+
     }
+    public void QuitGame()
+     {
+        Application.Quit();
+
+     }
 }
